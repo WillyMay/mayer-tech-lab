458223571e06711aa774af9c64b21df01427ad75
WillyMay <willian.mayer@proton.me>
2024-06-22 14:18:15 -0400

    finishing touches

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -8,17 +8,17 @@ function Questions() {
           Preguntas frequentes
         </h2>
       </div>
-      <div class="bg-white border border-gray-200 divide-y divide-gray-200 rounded-xl m-12">
-        <details class="p-6 group">
-          <summary class="flex items-center justify-between cursor-pointer">
-            <h5 class="text-2xl font-medium text-gray-900">
+      <div className="bg-white border border-gray-200 divide-y divide-gray-200 rounded-xl m-12">
+        <details className="p-6 group">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h5 className="text-2xl font-medium text-gray-900">
               ¿Cuánto tiempo demora en estar mi página web lista?
             </h5>
 
-            <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+            <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -33,7 +33,7 @@ function Questions() {
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -48,22 +48,22 @@ function Questions() {
             </span>
           </summary>
 
-          <p class="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
+          <p className="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
             Tu sitio web puede estar listo alrededor de 15 días, dependiendo de
             los requerimientos de la misma.
           </p>
         </details>
 
-        <details class="p-6 group">
-          <summary class="flex items-center justify-between cursor-pointer">
-            <h5 class="text-2xl font-medium text-gray-900">
+        <details className="p-6 group">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h5 className="text-2xl font-medium text-gray-900">
               ¿Se paga una sola vez por año?
             </h5>
 
-            <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+            <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -78,7 +78,7 @@ function Questions() {
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -93,23 +93,23 @@ function Questions() {
             </span>
           </summary>
 
-          <p class="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
+          <p className="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
             ¡Sí! Solo pagas una sola vez por año por tu página web. Después de
             un año tiene un costo de $50.000 para permanecer en línea por otro
             año más.
           </p>
         </details>
 
-        <details class="p-6 group">
-          <summary class="flex items-center justify-between cursor-pointer">
-            <h5 class="text-2xl font-medium text-gray-900">
+        <details className="p-6 group">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h5 className="text-2xl font-medium text-gray-900">
               ¿Tienen conexión a redes sociales?
             </h5>
 
-            <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+            <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -124,7 +124,7 @@ function Questions() {
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -139,22 +139,22 @@ function Questions() {
             </span>
           </summary>
 
-          <p class="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
+          <p className="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
             Todas las páginas nuestras tienen conexiones a redes sociales por
             defecto, incluso link directo a WhatsApp.
           </p>
         </details>
-        <details class="p-6 group">
-          <summary class="flex items-center justify-between cursor-pointer">
-            <h5 class="text-2xl font-medium text-gray-900">
+        <details className="p-6 group">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h5 className="text-2xl font-medium text-gray-900">
               ¿Puedo cambiar la extensión de mi pagina web a otro en vez de
               ".com"?
             </h5>
 
-            <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+            <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -169,7 +169,7 @@ function Questions() {
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -184,22 +184,22 @@ function Questions() {
             </span>
           </summary>
 
-          <p class="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
+          <p className="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
             ¡Por supuesto! Puedes cambiar la extensión de tu página web a ".cl",
             ".co", ".ar", etc. Pero tiene un costo adicional de $10.000, único
             pago.
           </p>
         </details>
-        <details class="p-6 group">
-          <summary class="flex items-center justify-between cursor-pointer">
-            <h5 class="text-2xl font-medium text-gray-900">
+        <details className="p-6 group">
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h5 className="text-2xl font-medium text-gray-900">
               ¿Cual es la diferencia entre dominio y hosting?
             </h5>
 
-            <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+            <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -214,7 +214,7 @@ function Questions() {
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -229,7 +229,7 @@ function Questions() {
             </span>
           </summary>
 
-          <p class="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
+          <p className="mt-4 text-lg leading-relaxed text-gray-700 font-inter">
             El hosting es un espacio limitado en el internet para poder colocar
             tu sitio web. En cambio, el hosting es la dirección con el cual
             ingresas al dominio, como la dirección a una casa.
--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 function Whatsapp() {
   return (
-    <div id="contacto" class="bg-gray-100 dark:bg-gray-800">
-      <div class="z-20 w-full px-4 py-12 mx-auto text-center sm:px-6 lg:py-16 lg:px-8">
-        <h2 class="text-black dark:text-white sm:text-4xl">
-          <span class="text-base font-semibold leading-6 text-gray-900 uppercase">
+    <div id="contacto" className="bg-gray-100 dark:bg-gray-800">
+      <div className="z-20 w-full px-4 py-12 mx-auto text-center sm:px-6 lg:py-16 lg:px-8">
+        <h2 className="text-black dark:text-white sm:text-4xl">
+          <span className="text-base font-semibold leading-6 text-gray-900 uppercase">
             ¿Algunas preguntas?
           </span>
-          <span class="block mt-2 text-2xl font-extrabold leading-8 text-gray-900 dark:text-white sm:text-3xl sm:leading-9">
+          <span className="block mt-2 text-2xl font-extrabold leading-8 text-gray-900 dark:text-white sm:text-3xl sm:leading-9">
             Contáctanos a través de nuestro WhatsApp personal{" "}
           </span>
         </h2>
 
-        <div class="lg:mt-0 lg:flex-shrink-0">
-          <div class="inline-flex mt-12 rounded-md shadow">
+        <div className="lg:mt-0 lg:flex-shrink-0">
+          <div className="inline-flex mt-12 rounded-md shadow">
             <a
               href="https://wa.link/5q8myl"
-              class="w-full px-6 py-4 text-base font-semibold text-center text-white transition duration-200 ease-in bg-green-600 rounded-lg shadow-md hover:bg-green-900 focus:ring-lime-900 focus:ring-offset-lime-900 focus:outline-none focus:ring-2 focus:ring-offset-2 "
+              className="w-full px-6 py-4 text-base font-semibold text-center text-white transition duration-200 ease-in bg-green-600 rounded-lg shadow-md hover:bg-green-900 focus:ring-lime-900 focus:ring-offset-lime-900 focus:outline-none focus:ring-2 focus:ring-offset-2 "
             >
               Presiona aquí
             </a>
